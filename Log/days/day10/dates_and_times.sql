a45c78c246ee7be27baa3590264107d958ffa4d4
Oyebamiji Micheal Tomiwa <oyebamijimicheal10@gmail.com>
2022-10-05 12:16:28 +0100

    reformat queries

--- Log/days/day10/dates_and_times.sql
+++ Log/days/day10/dates_and_times.sql
@@ -7,15 +7,15 @@ USE test_db;
 -- create a new table 
 CREATE TABLE patients(
 	patient_name VARCHAR(50),
-            vaccine_date DATE,
-            vaccine_time TIME,
-            vaccine_datetime DATETIME
-            );
+    vaccine_date DATE,
+    vaccine_time TIME,
+    vaccine_datetime DATETIME
+);
 
 -- insert values into patients table
 INSERT INTO 
 	patients(patient_name, vaccine_date, vaccine_time, vaccine_datetime)
 VALUES 
 	('Micheal', '2022-03-20', '10:32:00', '2022-03-20 10:32:00'),
-            ('Oyebamiji', '2022-06-12', '06:14:41', '2022-06-12 06:14:41'),
-            ('Tomiwa', '2021-12-12', '14:28:20', '2021-12-12 14:28:20');
+    ('Oyebamiji', '2022-06-12', '06:14:41', '2022-06-12 06:14:41'),
+    ('Tomiwa', '2021-12-12', '14:28:20', '2021-12-12 14:28:20');
